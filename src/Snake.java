1e914f805ae98d877ac1b04358dbd11159a6aa1f
Nikhil <Nikhil@Nikhil-PC>
2016-09-17 01:18:17 +0530

    new commit

---
+++ src/Snake.java
@@ -0,0 +1,197 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nikhil
+ */
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import java.util.ArrayList;
+import java.util.Random;
+import sun.audio.*;
+
+/**
+ *
+ * @author Nikhil
+ */
+public class Snake implements ActionListener, KeyListener {
+
+    public static Snake snake;
+    public JFrame jframe;
+    public RenderPanel renderPanel;
+    public Timer timer = new Timer(20, this);
+    public ArrayList<Point> snakeParts = new ArrayList<Point>();
+    public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, SCALE = 10;
+    public int ticks = 0, direction = DOWN, score, tailLength = 10, time;
+    public Point head, cherry;
+    public Random random;
+    public boolean over = false, paused;
+    public Dimension dim;
+
+    Snake() {
+        dim = Toolkit.getDefaultToolkit().getScreenSize();
+        jframe = new JFrame("Snake");
+        jframe.setVisible(true);
+        jframe.setSize(805, 700);
+        jframe.setResizable(false);
+
+        jframe.setLocation(dim.width / 2 - jframe.getWidth() / 2, dim.height / 2 - jframe.getHeight() / 2);
+        jframe.add(renderPanel = new RenderPanel());
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.addKeyListener(this);
+        startGame();
+
+    }
+
+    public void startGame() {
+        paused = false;
+        over = false;
+        time = 0;
+        ticks = 0;
+        score = 0;
+        tailLength = 14;
+        snakeParts.clear();
+        direction = DOWN;
+        random = new Random();
+        head = new Point(0, -1);
+        cherry = new Point(random.nextInt(79), random.nextInt(66));
+        timer.start();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        renderPanel.repaint();
+        ticks++;
+
+        if (ticks % 2 == 0 && head != null && !over && !paused) {
+            time++;
+
+            snakeParts.add(new Point(head.x, head.y));
+
+            if (direction == UP) {
+                if (head.y - 1 >= 0 && noTailAt(head.x, head.y - 1)) {
+                    head = new Point(head.x, head.y - 1);
+                }
+                else if(head.y-1<0 &&  noTailAt(head.x, head.y - 1))
+                {
+                    head = new Point(head.x, 67-head.y - 1);
+                }
+                else {
+                    over = true;
+
+                }
+            }
+
+            if (direction == DOWN) {
+                if (head.y + 1 < 67 && noTailAt(head.x, head.y + 1)) {
+                    head = new Point(head.x, head.y + 1);
+                }
+                else if (head.y + 1 >= 67 && noTailAt(head.x, head.y + 1))
+                {
+                    head = new Point(head.x, 67-head.y);
+                }
+                else {
+                    over = true;
+                }
+            }
+
+            if (direction == LEFT) {
+                if (head.x - 1 >= 0 && noTailAt(head.x - 1, head.y)) {
+                    head = new Point(head.x - 1, head.y);
+                }
+                else if(head.x - 1 < 0 && noTailAt(head.x - 1, head.y))
+                {
+                     head = new Point(80+head.x - 1, head.y);
+                    
+                }
+                else {
+                    over = true;
+                }
+            }
+
+            if (direction == RIGHT) {
+                if (head.x + 1 <= 80 && noTailAt(head.x + 1, head.y)) {
+                    head = new Point(head.x + 1, head.y);
+                }
+                else if(head.x + 1 > 80 && noTailAt(head.x + 1, head.y))
+                {
+                   head = new Point(80-head.x, head.y); 
+                }
+                else {
+                    over = true;
+                }
+            }
+
+            if (snakeParts.size() > tailLength) {
+                snakeParts.remove(0);
+
+            }
+
+            if (cherry != null) {
+                if (head.equals(cherry)) {
+                    score += 10;
+                    tailLength++;
+                    cherry.setLocation(random.nextInt(79), random.nextInt(66));
+                }
+            }
+        }
+    }
+
+    public boolean noTailAt(int x, int y) {
+        for (Point point : snakeParts) {
+            if (point.equals(new Point(x, y))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+ 
+    public static void main(String[] args) {
+        snake = new Snake();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int i = e.getKeyCode();
+
+        if ((i == KeyEvent.VK_A || i == KeyEvent.VK_LEFT) && direction != RIGHT) {
+            direction = LEFT;
+        }
+
+        if ((i == KeyEvent.VK_D || i == KeyEvent.VK_RIGHT) && direction != LEFT) {
+            direction = RIGHT;
+        }
+
+        if ((i == KeyEvent.VK_W || i == KeyEvent.VK_UP) && direction != DOWN) {
+            direction = UP;
+        }
+
+        if ((i == KeyEvent.VK_S || i == KeyEvent.VK_DOWN) && direction != UP) {
+            direction = DOWN;
+        }
+
+        if (i == KeyEvent.VK_SPACE) {
+            if (over) {
+                startGame();
+            } else {
+                paused = !paused;
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+}
